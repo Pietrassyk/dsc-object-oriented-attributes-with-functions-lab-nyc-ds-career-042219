b2baa592a2cc1b3eec1bdb040261dbb79e09d557
Tino Pietrassyk <pietrassyk@googlemail.com>
2019-05-13 15:49:17 -0400

    Finished Lab

--- school.py
+++ school.py
@@ -1,1 +1,21 @@
 class School:
+    def __init__(self, name, roster = {}):
+        self.name = name
+        self.roster = roster
+
+    def add_student (self,full_name, grade):
+        if grade not in self.roster.keys():
+            self.roster.update({grade:[full_name]})
+        else:
+            self.roster.update({grade: self.roster[grade]+[full_name]})
+
+    def grade(self,grade):
+        return self.roster[grade]
+
+    def sort_roster(self):
+        for key in self.roster.keys():
+            self.roster[key] = sorted(self.roster[key])
+        return self.roster
+
+
+
